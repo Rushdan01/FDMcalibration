2ef62360fef12cb66470018f25af73adbdede8ba
Rushdan01 <rushdanmikail3@gmail.com>
2026-04-13 12:07:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ true_lengths = []
 measured_lengths = []
 
 st.write("\nHow many cross measurements do you want to enter?")
-n = int(input(">> "))
+n = int(st.text_area("->"))
 
 for i in range(n):
     st.write(f"\nMeasurement {i+1}")
