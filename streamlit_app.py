d694d41b4e1e2c4500f9ecfea41b9969d746ae57
Rushdan01 <rushdanmikail3@gmail.com>
2026-04-13 11:35:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("FDM Calibration Tool")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Print the Calibraation Cross and input Expected values and Actual Values"
 )
